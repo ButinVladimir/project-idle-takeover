fce46dc24dd50f0c5d9f5abec26e68f556ec0838
Vladimir Butin <15980943+ButinVladimir@users.noreply.github.com>
2025-12-27 22:37:58 +0100

    #85 balance

--- src/texts/clone-templates.ts
+++ src/texts/clone-templates.ts
@@ -13,4 +13,16 @@ export const CLONE_TEMPLATE_TEXTS: Record<
       msg(`Leftover biomass fused with crude implants.
 Can do everything but without outstanding results.`),
   },
+  fixer: {
+    title: () => msg('Fixer'),
+    overview: () => msg(`Clone, specialized on negotiaing and dealing with large amount of information.`),
+  },
+  bouncer: {
+    title: () => msg('Bouncer'),
+    overview: () => msg(`Clone, specialized on close combat.`),
+  },
+  gunner: {
+    title: () => msg('Gunner'),
+    overview: () => msg(`Clone, specialized on ranged combat.`),
+  },
 };
--- src/texts/contracts.ts
+++ src/texts/contracts.ts
@@ -9,6 +9,10 @@ export const CONTRACT_TEXTS: Record<
 > = {
   security: {
     title: () => msg('Security'),
-    overview: () => msg(`Performing security duties.`),
+    overview: () => msg(`Perform security duties.`),
+  },
+  backroomDeal: {
+    title: () => msg('Backroom deal'),
+    overview: () => msg('Strike a deal to gain advantage.'),
   },
 };
